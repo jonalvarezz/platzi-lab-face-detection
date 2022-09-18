dc4b3eba6541b83d02510130afcfdcc30b13e672
Jonathan Alvarez <j@jonalvarezz.com>
2022-09-18 22:30:59 +0200

    add facedetector controller
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

---
+++ src/controllers/FaceDetector.js
@@ -0,0 +1,66 @@
+import "@mediapipe/face_detection";
+import "@tensorflow/tfjs-core";
+// Register WebGL backend.
+import "@tensorflow/tfjs-backend-webgl";
+import * as faceDetection from "@tensorflow-models/face-detection";
+
+import { Logger } from "../utils/log";
+
+const logger = new Logger("detector");
+
+export class FaceDetector {
+  /**
+   * @property {import("@tensorflow-models/face-detection").FaceDetector} detector
+   */
+  detector = null;
+  /**
+   * @property {HTMLVideoElement | HTMLImageElement}
+   */
+  media = null;
+
+  /**
+   * @param {HTMLVideoElement | HTMLImageElement} media
+   */
+  constructor(media) {
+    this.media = media;
+  }
+
+  /**
+   *
+   * @param {Object} options
+   * @param {number} options.maxFaces - Maximum number of faces to detect
+   *
+   * @returns {Promise<FaceDetector>}
+   */
+  async init({ maxFaces = 5 } = {}) {
+    logger.log(`Initializing face detector with ${maxFaces} max faces`);
+    const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
+    const detectorConfig = {
+      maxFaces,
+      runtime: "mediapipe",
+      solutionPath: "node_modules/@mediapipe/face_detection",
+    };
+
+    const detector = await faceDetection.createDetector(model, detectorConfig);
+    this.detector = detector;
+
+    return this;
+  }
+
+  async detect() {
+    if (!this.detector) {
+      throw new Error("Face detector not initialized");
+    }
+
+    logger.log("Detecting faces...");
+    const estimationConfig = { flipHorizontal: false };
+
+    const faces = await this.detector.estimateFaces(
+      this.media,
+      estimationConfig
+    );
+    logger.log(`Found ${faces.length} faces`);
+
+    return faces;
+  }
+}
