0b7929bf602d19f40b1b7a2c025eff9e6a7db9e8
Jonathan Alvarez <j@jonalvarezz.com>
2022-10-09 14:57:37 +0200

    facefilter:add controller to render filter collection
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

---
+++ src/controllers/FaceFilters.js
@@ -0,0 +1,54 @@
+import { Filter } from "./Filter";
+
+export class FaceFilters {
+  /**
+   * @type {Array<Filter>} Collection of filters
+   */
+  filters = [];
+
+  /**
+   * @type {HTMLCanvasElement} canvas
+   */
+  canvas = null;
+  /**
+   * @type {CanvasRenderingContext2D} canvas' context
+   */
+  canvasContext = null;
+
+  /**
+   * @param {HTMLCanvasElement} canvas
+   * @param {Array<Filter>} filters
+   */
+  constructor(canvas, filters) {
+    this.canvas = canvas;
+    this.canvasContext = canvas.getContext("2d");
+    this.filters = filters.map(this.validateFilter);
+  }
+
+  render(faces) {
+    this.clear();
+    faces.forEach((face) => {
+      this.filters.forEach((filter) => filter.draw(this.canvasContext, face));
+    });
+  }
+
+  /**
+   *
+   * @param {Filter} filter
+   * @returns Filter
+   */
+  validateFilter = (filter) => {
+    if (!(filter instanceof Filter)) {
+      throw new Error("Invalid filter", filter);
+    }
+
+    return filter;
+  };
+
+  /**
+   * Clear the canvas from any drew filters
+   */
+  clear() {
+    this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
