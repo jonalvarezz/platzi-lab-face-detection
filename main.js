a05dd9efb8dd2270a6872553986743d12ea0cf31
Jonathan Alvarez <j@jonalvarezz.com>
2022-10-09 15:37:34 +0200

    app: setup and initialize for img
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 import "./src/plugins";
 
 import { FaceDetector } from "./src/controllers/FaceDetector";
+import { UserMedia } from "./src/controllers/UserMedia";
+import { FaceFilters } from "./src/controllers/FaceFilters";
 
 import { MeshOption } from "./src/components/MeshOption";
 import { KeypointsOption } from "./src/components/KeypointsOption";
 import { BoundingBoxOption } from "./src/components/BoundingBoxOption";
 
+// Set up the user media
+const userMedia = new UserMedia(document.querySelector(".js-media-container"), {
+  type: "image",
+  src: "platzi.jpg",
+  width: 640,
+  height: 480,
+  onCanvasCreated: (canvas) => {
+    canvas.className = "absolute top-0 left-0 w-full h-full";
+    return canvas;
+  },
+});
+
+const faceDetector = new FaceDetector(userMedia.getMedia());
+
+// Set up options/ filters
 const optionsContainer = document.querySelector("#js-options");
 
 const mesh = new MeshOption("mesh", "Mesh", optionsContainer);
@@ -19,3 +36,14 @@ const boundingBox = new BoundingBoxOption(
   "Bounding box",
   optionsContainer
 );
+
+const filters = [mesh, keypoints, boundingBox];
+const faceFilters = new FaceFilters(userMedia.getCanvas(), filters);
+
+// Start the app
+async function app() {
+  await Promise.all([userMedia.init(), faceDetector.init()]);
+
+  faceDetector.subscribe(faceFilters.render.bind(faceFilters));
+}
+app();
