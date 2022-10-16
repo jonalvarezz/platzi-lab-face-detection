41beeb3dd2c7e539c61fe81080f7bf419db3fa21
Jonathan Alvarez <j@jonalvarezz.com>
2022-10-16 19:28:05 +0200

    fix tailwind to listen for main js
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.js"],
+  content: ["./index.html", "./main.js", "./src/**/*.js"],
   theme: {
     extend: {},
   },
