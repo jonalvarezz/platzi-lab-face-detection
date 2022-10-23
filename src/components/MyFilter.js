bbd18952578bebf9cb66686649765cb8f6013cbd
Jonathan Alvarez <j@jonalvarezz.com>
2022-10-23 15:23:46 +0200

    MyFilter: include index for itsreference
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- src/components/MyFilter.js
+++ src/components/MyFilter.js
@@ -5,7 +5,9 @@ export class MyFilter extends Option {
    * Draw the filter on the canvas
    * @param {CanvasRenderingContext2D} canvasContext
    * @param {import("@tensorflow-models/face-detection").Face} face
+   * @param {number} index
    */
+  draw(canvasContext, face, index) {
     if (!this.isEnabled()) {
       return this;
     }
